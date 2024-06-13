c9160e56030c7522bd1325434fd271a6ef8d7d18
Keven Augusto <kevenbezerra@gmail.com>
2024-06-13 15:23:32 -0400

    Initial commit

---
+++ src/test/java/br/com/kevenaugusto/miniauthorizer/MiniAuthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kevenaugusto.miniauthorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAuthorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
