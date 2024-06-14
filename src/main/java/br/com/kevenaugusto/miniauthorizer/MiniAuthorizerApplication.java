99cdf4bf46689d23f306797034a486e160b2656d
Keven Augusto <kevenbezerra@gmail.com>
2024-06-14 15:29:50 -0400

    docs: generate swagger documentation for api

--- src/main/java/br/com/kevenaugusto/miniauthorizer/MiniAuthorizerApplication.java
+++ src/main/java/br/com/kevenaugusto/miniauthorizer/MiniAuthorizerApplication.java
@@ -1,9 +1,12 @@
 package br.com.kevenaugusto.miniauthorizer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Mini Autorizador", version = "0.0.1", description = "Autorizador para transações de Vale Refeição e Vale Alimentação."))
 public class MiniAuthorizerApplication {
 
 	public static void main(String[] args) {
