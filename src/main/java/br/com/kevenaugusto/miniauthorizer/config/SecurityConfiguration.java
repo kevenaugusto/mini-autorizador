a0c56d8aea85d294f0aa98faced450e3584a0fc5
Keven Augusto <kevenbezerra@gmail.com>
2024-06-13 20:11:28 -0400

    feat: configure security filter chain
    Disable Cross Site Request Forgery and authorize requests to '/cartoes' only if user has role ROLE_USER

---
+++ src/main/java/br/com/kevenaugusto/miniauthorizer/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package br.com.kevenaugusto.miniauthorizer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((authorize) -> authorize
+            .requestMatchers("/cartoes/**").hasRole("USER")
+            .anyRequest().authenticated()
+        );
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+}
