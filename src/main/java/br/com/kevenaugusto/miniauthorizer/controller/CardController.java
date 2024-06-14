bb0af034d6bdbade81a93d9030f9009e25533cb3
Keven Augusto <kevenbezerra@gmail.com>
2024-06-14 00:59:08 -0400

    feat: map card balance endpoint

--- src/main/java/br/com/kevenaugusto/miniauthorizer/controller/CardController.java
+++ src/main/java/br/com/kevenaugusto/miniauthorizer/controller/CardController.java
@@ -7,9 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 public class CardController {
@@ -20,6 +20,11 @@ public class CardController {
     @PostMapping("/cartoes")
     public ResponseEntity<CardResponseDto> createCard(@RequestBody @Valid InputCardDto inputCardDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(cardService.createCard(inputCardDto));
+    }
+
+    @GetMapping("/cartoes/{cardNumber}")
+    public ResponseEntity<BigDecimal> getCardBalance(@PathVariable(value = "cardNumber") String cardNumber) {
+        return ResponseEntity.status(HttpStatus.OK).body(cardService.getCardBalance(cardNumber));
     }
 
 }
